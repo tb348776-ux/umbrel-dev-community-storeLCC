a6860d73663652e321fb7b0e010df3b7a3691b6b
WillItMod <dev@willitmod.local>
2026-01-05 01:49:34 +0000

    Promote AxeDGB 0.8.71 + AxeBCH 0.7.62 to dev channel

--- willitmod-dev-dgb/data/templates/dgb-entrypoint.sh
+++ willitmod-dev-dgb/data/templates/dgb-entrypoint.sh
@@ -15,5 +15,27 @@ if [ -f /data/.reindex-chainstate ]; then
   extra="-reindex-chainstate"
 fi
 
+dbcache="${DGB_DBCACHE_MB:-}"
+if [ -z "$dbcache" ] && [ -r /proc/meminfo ]; then
+  mem_kb="$(awk '/^MemTotal:/ {print $2}' /proc/meminfo 2>/dev/null || true)"
+  if [ -n "$mem_kb" ]; then
+    if [ "$mem_kb" -le 1048576 ]; then
+      dbcache="128"
+    elif [ "$mem_kb" -le 2097152 ]; then
+      dbcache="256"
+    elif [ "$mem_kb" -le 4194304 ]; then
+      dbcache="512"
+    elif [ "$mem_kb" -le 8388608 ]; then
+      dbcache="1024"
+    else
+      dbcache="2048"
+    fi
+  fi
+fi
+
+if [ -n "$dbcache" ]; then
+  extra="$extra -dbcache=$dbcache"
+fi
+
 echo "[axedgb] Exec: digibyted -datadir=/data -printtoconsole $extra"
 exec digibyted -datadir=/data -printtoconsole $extra
